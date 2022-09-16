64bcccedaac1d50f0fbe7e89f4e236610664dae8
Shreyas Joshi <kit.shraysjoshi@gmail.com>
2022-09-16 23:01:17 +0530

    DGTL-101: dependancy added for test ng

--- src/main/java/com/myecom/utility/Utility.java
+++ src/main/java/com/myecom/utility/Utility.java
@@ -25,7 +25,7 @@ public class Utility extends BaseClass {
 	public void applyExplicitWait(WebDriver driver, WebElement ele) {
 		
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-		wait.until(ExpectedConditions.elementToBeClickable(ele));
+		wait.until(ExpectedConditions.visibilityOf(ele));
 		
 	}
 	
